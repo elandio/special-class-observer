2e6801a5fafd06c64e7a50280d490b5b763ca746
m.duettmann@gmail.com <m.duettmann@gmail.com@da0aeb96-5df8-bbe2-962e-7361e4b4b254>
2012-03-11 21:32:38 +0000

    Initial import

---
+++ trunk/project/src/edu/uwp/cs/android/sco/ClassListActivity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.uwp.cs.android.sco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * @author M.Duettmann
+ *
+ */
+public class ClassListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
