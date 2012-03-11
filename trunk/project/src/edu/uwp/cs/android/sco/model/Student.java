46305ef2f5b318b19bd063e71d70463ea8102d17
m.duettmann@gmail.com <m.duettmann@gmail.com@da0aeb96-5df8-bbe2-962e-7361e4b4b254>
2012-03-11 21:45:43 +0000

    Initial import

--- trunk/project/src/edu/uwp/cs/android/sco/model/Student.java
+++ trunk/project/src/edu/uwp/cs/android/sco/model/Student.java
@@ -3,6 +3,9 @@
  */
 package edu.uwp.cs.android.sco.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Student model
@@ -11,5 +14,53 @@ package edu.uwp.cs.android.sco.model;
  *
  */
 public class Student {
+    
+    private String lName;  // Last Name
+    private String fName;  // First Name
+    
+    private Map<String, Integer> attrs = new HashMap<String, Integer>();  // Attributes
+    
+    
+    public void Student(String fName, String lName){
+        this.fName = fName;
+        this.lName = lName;
+        
+    }
+
+
+    
+    /**
+     * @return the lName
+     */
+    public String getlName() {
+        return lName;
+    }
+
+
+    
+    /**
+     * @param lName the lName to set
+     */
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+
+    
+    /**
+     * @return the fName
+     */
+    public String getfName() {
+        return fName;
+    }
+
+
+    
+    /**
+     * @param fName the fName to set
+     */
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
 
 }
