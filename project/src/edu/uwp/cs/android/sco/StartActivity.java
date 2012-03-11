9d9114794c05ea154fbb3e5a59a36c0acaf5fe26
m.duettmann <m.duettmann@gmail.com>
2012-03-11 21:22:08 +0000

    Initial import

---
+++ project/src/edu/uwp/cs/android/sco/StartActivity.java
@@ -0,0 +1,13 @@
+package edu.uwp.cs.android.sco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
