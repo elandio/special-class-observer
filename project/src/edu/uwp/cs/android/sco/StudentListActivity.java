a45f51f1dcf660ebec5408b30df666541c308b2d
m.duettmann <m.duettmann@googlemail.com>
2012-03-17 23:14:13 +0000

    AddClass & AddStudent work so far - But new entrys are not saved over the activity borders!!! Solution propably with SQL lite DB.

--- project/src/edu/uwp/cs/android/sco/StudentListActivity.java
+++ project/src/edu/uwp/cs/android/sco/StudentListActivity.java
@@ -4,6 +4,7 @@
 package edu.uwp.cs.android.sco;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.database.DataSetObserver;
 import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
 import android.os.Bundle;
@@ -15,12 +16,14 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.View.OnKeyListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
+import edu.uwp.cs.android.sco.model.Student;
 import edu.uwp.cs.android.sco.model.StudentList;
 
 
@@ -34,6 +37,7 @@ public class StudentListActivity extends Activity {
     private TextView tView;
     private EditText searchEdit;
     private ArrayAdapter<String> adapter;
+    private Button addStundentButton;
     
     /** Called when the activity is first created. */
     @Override
@@ -44,10 +48,11 @@ public class StudentListActivity extends Activity {
         lView = (ListView) findViewById(R.id.stList);
         tView = (TextView) findViewById(R.id.className);
         searchEdit = (EditText) findViewById(R.id.searchStudentList);
+        addStundentButton = (Button) findViewById(R.id.addStudentButton);
 
         System.out.println(getIntent().getSerializableExtra("Selected_Class"));
         
-        StudentList stList = (StudentList) getIntent().getSerializableExtra("Selected_Class");
+        final StudentList stList = (StudentList) getIntent().getSerializableExtra("Selected_Class");
         tView.setText(stList.toString());
         
         // First paramenter - Context
@@ -60,7 +65,7 @@ public class StudentListActivity extends Activity {
         // Assign adapter to ListView
         lView.setAdapter(adapter);
         
-        TextWatcher changeListener = new TextWatcher() {
+        searchEdit.addTextChangedListener(new TextWatcher() {
             
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) { 
@@ -80,16 +85,62 @@ public class StudentListActivity extends Activity {
                 // TODO Auto-generated method stub
                 
             }
-        };
+        });
         
-        OnClickListener onClickListener = new OnClickListener() {
+        searchEdit.setOnClickListener( new OnClickListener() {
             
             @Override
             public void onClick(View v) {
                 searchEdit.setText("");
             }
-        };
-        searchEdit.setOnClickListener(onClickListener);
-        searchEdit.addTextChangedListener(changeListener);
+        });
+        
+        
+        
+        addStundentButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                final Dialog addClassDialog = new Dialog(StudentListActivity.this);
+               
+                addClassDialog.setContentView(R.layout.add_student);
+                addClassDialog.setTitle("Add a student");
+                addClassDialog.setCancelable(true);
+                
+                addClassDialog.show();
+                
+                
+                final EditText fNameText = (EditText) addClassDialog.findViewById(R.id.studentFNameEditText);
+                final EditText lNameText = (EditText) addClassDialog.findViewById(R.id.studentLNameEditText);
+                Button cancelButton = (Button) addClassDialog.findViewById(R.id.cancelButton);
+                Button saveButton = (Button) addClassDialog.findViewById(R.id.saveButton);
+                
+                cancelButton.setOnClickListener(new OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v) {
+                        addClassDialog.dismiss();
+                    }
+                });
+                
+                saveButton.setOnClickListener(new OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v) {
+                        stList.add(new Student(fNameText.getText().toString(), lNameText.getText().toString()));
+//                        adapter.notifyDataSetChanged();
+//                        lView.invalidateViews();
+                        //TODO: Not perfect better update then creating a new adapter but notofiyDataSetChanged() is not working now....
+                        adapter = new ArrayAdapter<String>(StudentListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, stList.getStudentsNames());
+                        // Assign adapter to ListView
+                        lView.setAdapter(adapter);
+                        
+                        addClassDialog.dismiss();
+                    }
+                });
+                
+                
+            }
+        }); 
     }
 }
