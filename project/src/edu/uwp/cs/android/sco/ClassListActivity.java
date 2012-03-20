c321c89fcb14b13ad2bf5527035287661bca2b67
m.duettmann <m.duettmann@googlemail.com>
2012-03-20 23:09:10 +0000

    Prototyp of SQLite implementation for classes!

--- project/src/edu/uwp/cs/android/sco/ClassListActivity.java
+++ project/src/edu/uwp/cs/android/sco/ClassListActivity.java
@@ -3,6 +3,8 @@
  */
 package edu.uwp.cs.android.sco;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
@@ -21,6 +23,7 @@ import android.widget.ListView;
 import edu.uwp.cs.android.sco.model.ClassList;
 import edu.uwp.cs.android.sco.model.Student;
 import edu.uwp.cs.android.sco.model.StudentList;
+import edu.uwp.cs.android.sco.sqllite.ClassesDB;
 
 /**
  * @author M.Duettmann
@@ -33,9 +36,12 @@ public class ClassListActivity extends Activity {
 
     private EditText searchEdit;
 
-    private ArrayAdapter<String> adapter;
+//    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<StudentList> adapter;
     
     private Button addClass;
+    
+    private ClassesDB classesDB;
     
 
     /** Called when the activity is first created. */
@@ -47,30 +53,43 @@ public class ClassListActivity extends Activity {
         lView = (ListView) findViewById(R.id.clList);
         searchEdit = (EditText) findViewById(R.id.searchClassList);
         addClass = (Button) findViewById(R.id.addClassButton);
+        
+        classesDB = new ClassesDB(this);
+        classesDB.open();
+        
 
-        // Test Data
+
+        // Test Data 
         clList = new ClassList();
         Student me = new Student("Moritz", "Duettmann");
         Student you = new Student("Matthias", "Duettmann");
         Student he = new Student("Michael", "Duettmann");
         StudentList math_stList = new StudentList("Math-Class");
         StudentList eng_stList = new StudentList("Eng-Class");
+        
+//        StudentList test = classesDB.createStudentList("test");
+//        test.add(me);
+//        test.add(you);
+//        test.add(he);
+        
         math_stList.add(me);
         math_stList.add(you);
         math_stList.add(he);
-
+        
         clList.add(math_stList);
 
         clList.add(eng_stList);
 
+        List<StudentList> stuList = classesDB.getAllClasses();
+        
         lView.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 StudentList stList = clList.get(position);
-                System.out.println(stList.size());
-
+                
+                
                 Intent i = new Intent(parent.getContext(), StudentListActivity.class);
                 i.putExtra("Selected_Class", stList);
                 startActivity(i);
@@ -81,7 +100,10 @@ public class ClassListActivity extends Activity {
         // Second parameter - Layout for the row
         // Third parameter - ID of the View to which the data is written
         // Forth - the Array of data        
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, clList.getClassesNames());
+//        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, clList.getClassesNames());
+        
+        adapter = new ArrayAdapter<StudentList>(this,
+                android.R.layout.simple_list_item_1, stuList);
 
         // Assign adapter to ListView
         lView.setAdapter(adapter);
@@ -148,11 +170,16 @@ public class ClassListActivity extends Activity {
                     public void onClick(View v) {
                         clList.add(new StudentList(newClassText.getText().toString()));
                         
+                        StudentList test = classesDB.createStudentList(newClassText.getText().toString());
+                        
 //                        adapter.notifyDataSetChanged();
 //                        lView.invalidateViews();
                         
                         //TODO: Not perfect better update then creating a new adapter but notofiyDataSetChanged() is not working now....
-                        adapter = new ArrayAdapter<String>(ClassListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, clList.getClassesNames());
+//                        adapter = new ArrayAdapter<String>(ClassListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, clList.getClassesNames());
+                        List<StudentList> stuList = classesDB.getAllClasses();
+                        adapter = new ArrayAdapter<StudentList>(ClassListActivity.this,
+                                android.R.layout.simple_list_item_1, stuList);
                         // Assign adapter to ListView
                         lView.setAdapter(adapter);
                         
@@ -165,5 +192,17 @@ public class ClassListActivity extends Activity {
             }
         }); 
 
+    }
+    
+    @Override
+    protected void onResume() {
+        classesDB.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        classesDB.close();
+        super.onPause();
     }
 }
