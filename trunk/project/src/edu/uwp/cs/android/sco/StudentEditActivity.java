e4616a75fe9f630f6f55155882af89eff3c10d3c
mi.tiede@gmail.com <mi.tiede@gmail.com@da0aeb96-5df8-bbe2-962e-7361e4b4b254>
2012-04-06 01:11:53 +0000

    cleaned project: - deleted unused classes and xml-files - fixed classes which used the deprecated classes, except classes using the StudentAdapter

--- trunk/project/src/edu/uwp/cs/android/sco/StudentEditActivity.java
+++ trunk/project/src/edu/uwp/cs/android/sco/StudentEditActivity.java
@@ -1,9 +1,14 @@
 package edu.uwp.cs.android.sco;
 
-import edu.uwp.cs.android.sco.model.Disability;
-import edu.uwp.cs.android.sco.model.Student;
+import edu.uwp.cs.android.sco.entities.DaoMaster;
+import edu.uwp.cs.android.sco.entities.DaoSession;
+import edu.uwp.cs.android.sco.entities.Disability;
+import edu.uwp.cs.android.sco.entities.Student;
+import edu.uwp.cs.android.sco.entities.StudentDao;
+import edu.uwp.cs.android.sco.entities.DaoMaster.DevOpenHelper;
 import edu.uwp.cs.android.sco.model.StudentAdapter;
 import android.app.ListActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -11,15 +16,37 @@ import android.widget.TextView;
 
 public class StudentEditActivity extends ListActivity {
 	
+	// database management
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private StudentDao studentDao;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.edit_student);
-	    Student test = new Student("Matthias", "Kleinert", "Schwul");
-	    test.addDisability("Test1", "Test1");
-	    Disability[] disabilities = new Disability[test.attrs.size()];
+        setContentView(R.layout.edit_student);
+        
+        DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "student-db", null);
+        db = helper.getWritableDatabase();
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+        studentDao = daoSession.getStudentDao();
+        
+	    Student student = studentDao.load(1l);
+	    
+	    // TEMP CREATING STUDENT
+		if (student == null) {
+			// student is not in database - create a test student
+			student = new Student(1l, "Test", "Driver");
+			studentDao.insert(student);
+			student.addDefaultDisabilities();
+			studentDao.update(student);
+		}
+
+		Disability[] disabilities = new Disability[student.getDisabilities().size()];
 	    int i=0;
-	    for (Disability disability : test.attrs) {
+	    for (Disability disability : student.getDisabilities()) {
 			disabilities[i]= disability;
 			i++;
 		}
@@ -30,7 +57,7 @@ public class StudentEditActivity extends ListActivity {
 	    View header = (View)getLayoutInflater().inflate(R.layout.student_header, null);
         listView.addHeaderView(header);
         TextView tvheader = (TextView)findViewById(R.id.tvhead);
-	    tvheader.setText(test.toString());
+	    tvheader.setText(student.toString());
 
         setListAdapter(adapter);
 	}
